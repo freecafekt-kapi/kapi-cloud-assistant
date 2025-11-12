88bc9323d91a20006f9a7bb76a189d019742132c
QuocThai <quocthai@QuocThais-MacBook-Pro.local>
2025-11-12 12:28:28 +0700

    fix: correct path to index.html

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.post("/post", upload.single("image"), async (req, res) => {
