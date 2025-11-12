9b12fae4b18eac66e2e1f4627135fd981ed2f36e
QuocThai <quocthai@QuocThais-MacBook-Pro.local>
2025-11-12 12:58:24 +0700

    fix: force HTML content-type for index.html

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
+  res.setHeader("Content-Type", "text/html; charset=utf-8");
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
